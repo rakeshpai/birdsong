b9faa3e9396381be09945253c0072691ce432251
Rakesh Pai <rakeshpai@gmail.com>
2022-05-05 23:21:55 +0530

    Service type contains never

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-const add = (a: number, b: number) => ({ a, b });
+const add = (a: number, b: number) => Promise.resolve({ a, b });
 const concat = (a: string, b: string) => a + b;
 const addDates = (x: Date, y: Date) => x.getTime() + y.getTime();
 
@@ -16,9 +16,13 @@ type RPCFunction = {
 
 type RPCService<T extends Record<string, RPCFunction>> = {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [P in keyof T]: T[P] extends (...args: any[]) => Promise<infer U>
-    ? (...args: Parameters<T[P]>) => Promise<U>
-    : (...args: Parameters<T[P]>) => Promise<ReturnType<T[P]>>
+  readonly [P in keyof T]: T[P] extends (...args: any[]) => Promise<infer U>
+    ? Parameters<T[P]> extends RPCSerializableValue[]
+      ? (...args: Parameters<T[P]>) => Promise<U>
+      : never
+    : Parameters<T[P]> extends RPCSerializableValue[]
+      ? (...args: Parameters<T[P]>) => Promise<ReturnType<T[P]>>
+      : never
 };
 
 const toExpose = { add, concat, addDates };
