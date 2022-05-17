3ff66ddd54851ff0a8e689aeb6a02ab5d8c06443
Rakesh Pai <rakeshpai@gmail.com>
2022-05-17 21:02:20 +0530

    Ensuring serialisable

--- server.ts
+++ server.ts
@@ -1,6 +1,4 @@
-const add = (a: number, b: number) => Promise.resolve({ a, b });
-const concat = (a: string, b: string) => a + b;
-const addDates = (x: Date, y: Date) => x.getTime() + y.getTime();
+type MaybeAsync<T> = T | Promise<T>;
 
 type RPCSerializableValue =
     | { [property: string]: RPCSerializableValue }
@@ -10,20 +8,103 @@ type RPCSerializableValue =
     | boolean
     | null;
 
-type Promisify<T> = T extends Promise<infer U> ? Promise<U> : Promise<T>;
-
-type RPCFunction = (...args: RPCSerializableValue[]) => RPCSerializableValue | Promise<RPCSerializableValue>;
-
-type RPCService<T extends Record<string, RPCFunction>> = {
-  readonly [P in keyof T]: T[P] extends (...args: RPCSerializableValue[]) => Promise<infer U>
-    ? Parameters<T[P]> extends RPCSerializableValue[]
-      ? (...args: Parameters<T[P]>) => Promisify<U>
-      : never
-    : Parameters<T[P]> extends RPCSerializableValue[]
-      ? (...args: Parameters<T[P]>) => Promisify<ReturnType<T[P]>>
-      : never
+type ResolverArgs<Input extends RPCSerializableValue, Context> = {
+  input: Input;
+  context: Context;
 };
 
-const toExpose = { add, concat, addDates };
+type Validator<Input extends RPCSerializableValue> = (value: unknown) => MaybeAsync<Input>;
+type Resolver<
+  Input extends RPCSerializableValue,
+  Output extends RPCSerializableValue,
+  Context
+> = (x: ResolverArgs<Input, Context>) => MaybeAsync<Output>;
 
-export type MyServiceType = RPCService<typeof toExpose>;
+type OpArgs<
+  Input extends RPCSerializableValue,
+  Output extends RPCSerializableValue,
+  Context
+> = {
+  validator: Validator<Input>;
+  resolve: Resolver<Input, Output, Context>;
+};
+
+type ServiceMethodDescrptor<
+  Input extends RPCSerializableValue,
+  Output extends RPCSerializableValue,
+  Context
+> =
+  OpArgs<Input, Output, Context> & { type: 'query' | 'mutation' };
+
+const createDescriptor = (type: 'query' | 'mutation') => (
+  <Input extends RPCSerializableValue, Output extends RPCSerializableValue, Context>(
+    validator: Validator<Input>,
+    resolve: Resolver<Input, Output, Context>
+  ) => ({
+    type, validator, resolve
+  })
+);
+
+const query = createDescriptor('query');
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const mutation = createDescriptor('mutation');
+
+const createService = <
+  Input extends RPCSerializableValue,
+  Output extends RPCSerializableValue,
+  Context,
+  MethodNames extends string
+>(
+  createContext: () => Context,
+  methods: Record<MethodNames, ServiceMethodDescrptor<Input, Output, Context>>
+) => ({
+  createContext, methods
+});
+
+type Service<Context, SMD, methodName extends string> = {
+  createContext: () => Context;
+  methods: Record<
+    methodName,
+    SMD extends ServiceMethodDescrptor<infer Input, infer Output, Context>
+      ? ServiceMethodDescrptor<Input, Output, Context>
+      : never
+  >;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type ClientType<T extends Service<Context, ServiceMethodDescrptor<any, any, Context>, string>> = {
+  [methodName in keyof T['methods']]: (
+    T['methods'][methodName] extends ServiceMethodDescrptor<infer Input, infer Output, Context>
+      ? (a: Input) => Promise<Output>
+      : never
+  );
+};
+
+type Context = { isLoggedIn: boolean };
+
+const service = createService(() => ({ isLoggedIn: true }), {
+  getUser: query(
+    value => ({ userId: String(value || '') }),
+    ({ input, context }) => {
+      console.log(input, context.isLoggedIn);
+      return ({ name: 'John', surname: 'Doe' });
+    }
+  )
+});
+
+// Client
+
+const createClient = <T>() => new Proxy({}, {
+  get(target, prop) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    return (...args: any[]) => {
+      console.log('Invoking', prop, args);
+      return Promise.resolve(3);
+    };
+  }
+}) as T;
+
+type Client = ClientType<typeof service>;
+
+const c = createClient<Client>();
+c.getUser({ userId: '1' });
