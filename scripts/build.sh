b32f3f6d6899205ac581ff934608357202bd986c
Rakesh Pai <rakeshpai@gmail.com>
2022-06-03 22:45:42 +0530

    Gloriously silly mistake

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 tsc --build tsconfig.json
 tsc --build tsconfig.cjs.json
 vite build
+tsc src/client/index.ts --declaration --emitDeclarationOnly --jsx react --esModuleInterop --outDir dist
 rm -rf ./dist/server/cjs/src/client
 rm -rf ./dist/server/cjs/src/tests
 rm -rf ./dist/server/esm/src/client
--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,15 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const { defineConfig } = require('vite');
-const dts = require('vite-plugin-dts');
 
 module.exports = defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/client/index.ts'),
       name: 'birdsongHttpClient',
-      fileName: format => `client.${format}.js`
+      fileName: format => `index.${format}.js`
     },
     outDir: path.resolve(__dirname, 'dist/client'),
     sourcemap: true
-  },
-  plugins: [dts({ outputDir: path.resolve(__dirname, 'dist/client') })]
+  }
 });
